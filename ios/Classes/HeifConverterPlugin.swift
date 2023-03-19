c249363f0dadf58ad136b895271d61d80e28ad85
Giao Ho <joutvhu@gmail.com>
2023-03-20 02:05:45 +0700

    Convert for ios

--- ios/Classes/HeifConverterPlugin.swift
+++ ios/Classes/HeifConverterPlugin.swift
@@ -9,6 +9,21 @@ public class HeifConverterPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    if(call.method == "convert") {
+      let input = call.arguments as! Dictionary<String, Any>
+      let path = input["path"] as! String
+      let output = input["output"] as! String
+      result(convert(path, output))
+    }
+  }
+
+  func convert(path: String, output: String) -> String? {
+      let image : UIImage? = UIImage(named: path)
+      if image == nil {
+        return nil
+      }
+      let imageData = image!.jpegData(compressionQuality: 1.0)
+      FileManager.default.createFile(atPath: output, contents: imageData, attributes: nil)
+      return output
   }
 }
