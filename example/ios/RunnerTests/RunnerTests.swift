ceb9988373126db2f9fadc398e9de1f0fe02a066
Giao Ho <joutvhu@gmail.com>
2023-08-11 02:07:00 +0700

    Update example

---
+++ example/ios/RunnerTests/RunnerTests.swift
@@ -0,0 +1,26 @@
+import Flutter
+import UIKit
+import XCTest
+
+@testable import heif_converter
+
+// This demonstrates a simple unit test of the Swift portion of this plugin's implementation.
+//
+// See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+
+class RunnerTests: XCTestCase {
+
+  func testGetPlatformVersion() {
+    let plugin = HeifConverterPlugin()
+
+    let call = FlutterMethodCall(methodName: "getPlatformVersion", arguments: [])
+
+    let resultExpectation = expectation(description: "result block must be called.")
+    plugin.handle(call) { result in
+      XCTAssertEqual(result as! String, "iOS " + UIDevice.current.systemVersion)
+      resultExpectation.fulfill()
+    }
+    waitForExpectations(timeout: 1)
+  }
+
+}
