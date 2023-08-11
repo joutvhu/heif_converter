50493ad68fe119d253a621d0a7b8151ad1552a15
Giao Ho <joutvhu@gmail.com>
2023-08-12 02:15:18 +0700

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:future_progress_dialog/future_progress_dialog.dart';
 import 'package:heif_converter/heif_converter.dart';
 import 'package:path_provider/path_provider.dart';
 
